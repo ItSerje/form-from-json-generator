1f3088ca0863a535295e859c61914409fcd597fd
ItSerje <66359825+ItSerje@users.noreply.github.com>
2022-10-05 23:23:25 +0600

    build demo form as per tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { FC } from 'react';
-import './App.css';
 import { SignupForm } from './components/form/Form';
 
 const App: FC = () => {
--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,24 +1,121 @@
-import { useFormik } from 'formik';
+import { Formik, Form, useField } from 'formik';
+import * as Yup from 'yup';
 import styles from './form.module.scss';
 
-export const SignupForm = () => {
-  const formik = useFormik({
-    initialValues: { email: '' },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
+const TextInput = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor='email'>Email Address</label>
-      <input
-        id='email'
-        name='email'
-        type='email'
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
-      <button type='submit'>Submit</button>
-    </form>
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className={styles['text-input']} {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className={styles.error}>{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
+
+const Checkbox = ({ children, ...props }) => {
+  const [field, meta] = useField({ ...props, type: 'checkbox' });
+  return (
+    <div>
+      <label className={styles['checkbox-input']}>
+        <input type='checkbox' {...field} {...props} />
+        {children}
+      </label>
+      {meta.touched && meta.error ? (
+        <div className={styles.error}>{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
+
+const Select = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <div>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <select {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className={styles.error}>{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
+
+export const SignupForm = () => {
+  return (
+    <>
+      <h1>Subscribe!</h1>
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+          acceptedTerms: false,
+          jobType: '',
+        }}
+        validationSchema={Yup.object({
+          firstName: Yup.string()
+            .max(15, 'Must be 15 characters or less')
+            .required('Required'),
+          lastName: Yup.string()
+            .max(20, 'Must be 20 characters or less')
+            .required('Required'),
+          email: Yup.string()
+            .email('Invalid email address')
+            .required('Required'),
+          acceptedTerms: Yup.boolean()
+            .required('Required')
+            .oneOf([true], 'You must accept terms and conditions'),
+          jobType: Yup.string()
+            .oneOf(
+              ['designer', 'development', 'product', 'other'],
+              'Invalid Job Type'
+            )
+            .required('Required'),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        <Form>
+          <TextInput
+            label='First Name'
+            name='firstName'
+            type='text'
+            placeholder='John'
+          />
+          <TextInput
+            label='Last Name'
+            name='lastName'
+            type='text'
+            placeholder='Doe'
+          />
+          <TextInput
+            label='Email Address'
+            name='email'
+            type='email'
+            placeholder='john@doe.com'
+          />
+          <Select label='Job Type' name='jobType'>
+            <option value=''>Select a job type</option>
+            <option value='designer'>Designer</option>
+            <option value='development'>Development</option>
+            <option value='product'>Product</option>
+            <option value='other'>Other</option>
+          </Select>
+
+          <Checkbox name='acceptedTerms'>
+            I accept the terms and conditions
+          </Checkbox>
+
+          <button type='submit'>Submit</button>
+        </Form>
+      </Formik>
+    </>
   );
 };
