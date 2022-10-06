d30fa195c6bb7c4de33d9408b26a83b1eb85f8d0
ItSerje <66359825+ItSerje@users.noreply.github.com>
2022-10-06 21:06:57 +0600

    add NumberInput and CheckboxGroup

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -13,6 +13,11 @@ type CheckboxProps = JSX.IntrinsicElements['input'] & {
   name: string;
 };
 
+type CheckboxGroupProps = {
+  children: string[] | JSX.Element[];
+  groupLabel: string;
+};
+
 type SelectProps = JSX.IntrinsicElements['select'] & {
   label: string;
   name: string;
@@ -31,8 +36,44 @@ const TextInput = ({ label, ...props }: InputProps): JSX.Element => {
   );
 };
 
+const NumberInput = ({ label, ...props }: InputProps): JSX.Element => {
+  const [field, meta, helpers] = useField({ ...props, type: 'text' });
+
+  const increaseHandler = () => {
+    helpers.setValue(Number(field.value) + 1, true);
+  };
+  const decreaseHandler = () => {
+    if (Number(field.value) - 1 >= 0) {
+      helpers.setValue(Number(field.value) - 1, true);
+    }
+  };
+
+  return (
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <span onClick={decreaseHandler}>-</span>
+      <input
+        type='text'
+        className={styles['number-input']}
+        {...field}
+        onChange={(e) => {
+          if (/^\d+$/.test(e.target.value) || e.target.value === '') {
+            field.onChange(e);
+          }
+        }}
+        {...props}
+      />
+      <span onClick={increaseHandler}>+</span>
+      {meta.touched && meta.error ? (
+        <div className={styles.error}>{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
+
 const Checkbox = ({ children, ...props }: CheckboxProps): JSX.Element => {
   const [field, meta] = useField({ ...props, type: 'checkbox' });
+  console.log(field);
   return (
     <div>
       <label className={styles['checkbox-input']}>
@@ -43,6 +84,20 @@ const Checkbox = ({ children, ...props }: CheckboxProps): JSX.Element => {
         <div className={styles.error}>{meta.error}</div>
       ) : null}
     </div>
+  );
+};
+
+const CheckboxGroup = ({
+  groupLabel,
+  children,
+}: CheckboxGroupProps): JSX.Element => {
+  return (
+    <>
+      <label htmlFor='checkbox-group'>{groupLabel}</label>
+      <div id='checkbox-group' role='group' aria-labelledby='checkbox-group'>
+        {children}
+      </div>
+    </>
   );
 };
 
@@ -67,9 +122,11 @@ export const SignupForm: FC = () => {
         initialValues={{
           firstName: '',
           lastName: '',
+          age: '',
           email: '',
           acceptedTerms: false,
           jobType: '',
+          favourites: [],
         }}
         validationSchema={Yup.object({
           firstName: Yup.string()
@@ -78,6 +135,11 @@ export const SignupForm: FC = () => {
           lastName: Yup.string()
             .max(20, 'Must be 20 characters or less')
             .required('Required'),
+          age: Yup.number()
+            .required('Required')
+            .min(0, 'Invalid age')
+            .max(120, 'Wow, you are older than 120, but please still use 120.')
+            .integer('Only integers are allowed.'),
           email: Yup.string()
             .email('Invalid email address')
             .required('Required'),
@@ -90,6 +152,7 @@ export const SignupForm: FC = () => {
               'Invalid Job Type'
             )
             .required('Required'),
+          favourites: Yup.array().of(Yup.string()),
         })}
         onSubmit={(values, { setSubmitting }) => {
           setTimeout(() => {
@@ -111,12 +174,25 @@ export const SignupForm: FC = () => {
             type='text'
             placeholder='Doe'
           />
+
+          <NumberInput label='Age' name='age' min='0' max='120' />
+
           <TextInput
             label='Email Address'
             name='email'
             type='email'
             placeholder='john@doe.com'
           />
+
+          <CheckboxGroup groupLabel='Checkbox Group'>
+            <Checkbox name='favourites' value='Movies'>
+              Movies
+            </Checkbox>
+            <Checkbox name='favourites' value='Hobbies'>
+              Hobbies
+            </Checkbox>
+          </CheckboxGroup>
+
           <Select label='Job Type' name='jobType'>
             <option value=''>Select a job type</option>
             <option value='designer'>Designer</option>
