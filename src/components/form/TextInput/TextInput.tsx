3eb709c5badf6b2116281b1c586d9da05bbdcb3c
ItSerje <66359825+ItSerje@users.noreply.github.com>
2022-11-21 21:58:48 +0600

    clean up code

--- src/components/form/TextInput/TextInput.tsx
+++ src/components/form/TextInput/TextInput.tsx
@@ -1,31 +1,21 @@
 import { FC } from 'react';
 import { useField } from 'formik';
-import styles from '../form.module.scss';
-
-// export type TextInputProps = JSX.IntrinsicElements['input'] & {
-//   label: string;
-//   name: string;
-//   placeholder?: string;
-// };
 
 type TextInputProps = JSX.IntrinsicElements['input'] & {
   name: string;
 };
 
 const TextInput: FC<TextInputProps> = (props) => {
-  const [field, meta] = useField(props);
+  const [field] = useField(props);
 
   return (
-    <>
-      <input
-        // className={meta.error && meta.touched ? styles['validation-error'] : ''}
-        className='input'
-        spellCheck='false'
-        type={props.type || 'text'}
-        {...field}
-        {...props}
-      />
-    </>
+    <input
+      className='input'
+      spellCheck='false'
+      type={props.type || 'text'}
+      {...field}
+      {...props}
+    />
   );
 };
 
