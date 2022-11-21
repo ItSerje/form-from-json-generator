8198be6c59654d7a1beb1a95679dbc925b7f7055
ItSerje <66359825+ItSerje@users.noreply.github.com>
2022-11-21 19:40:36 +0600

    remove comments and not used imports

--- src/components/form/Checkbox/Checkbox.tsx
+++ src/components/form/Checkbox/Checkbox.tsx
@@ -1,12 +1,6 @@
-import { FieldMetaProps, FormikValues, useField } from 'formik';
-import { FC, Dispatch, SetStateAction, useEffect } from 'react';
+import { useField } from 'formik';
+import { FC } from 'react';
 import { FaCheck } from 'react-icons/fa';
-// import styles from '../form.module.scss';
-
-// type CheckboxProps = JSX.IntrinsicElements['input'] & {
-//   children: string | JSX.Element;
-//   name: string;
-// };
 
 type CheckboxProps = JSX.IntrinsicElements['input'] & {
   name: string;
@@ -18,11 +12,9 @@ const Checkbox: FC<CheckboxProps> = (props) => {
   const [field] = useField({ ...props, type: 'checkbox' });
 
   return (
-    // <div className='single-checkbox__wrapper'>
     <label className='single-checkbox__label'>
       <div className='single-checkbox__input-wrapper'>
         <input type='checkbox' className='single-checkbox__input' {...field} />
-
         <span>
           <FaCheck />
         </span>
