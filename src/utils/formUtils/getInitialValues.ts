9d2e09cf251610ea9a71d63031ad43f49590b40d
ItSerje <66359825+ItSerje@users.noreply.github.com>
2022-11-21 19:16:27 +0600

    update comments

--- src/utils/formUtils/getInitialValues.ts
+++ src/utils/formUtils/getInitialValues.ts
@@ -11,18 +11,13 @@ export const getInitialValues = (data: TFormData) => {
 
   data.steps.forEach((step) => {
     step.fields?.forEach((field) => {
+      // if initialValue is boolean itself, we need to make sure not to skip the below if
       if (field.initialValue || typeof field.initialValue === 'boolean') {
-        // if (typeof field.initialValue === 'boolean') {
-        //   initialValues[field.name] = field.initialValue;
-        // } else {
-        //   initialValues[field.name] = field.initialValue;
-        // }
         initialValues[field.name] = field.initialValue;
       } else {
         if (field.component === 'MultipleInputs') {
           initialValues[field.name] = [''];
         } else if (field.component === 'MultipleFilesUploadField') {
-          // initialValues[field.name] = [{ files: [] }];
           initialValues[field.name] = [];
         } else {
           initialValues[field.name] = '';
